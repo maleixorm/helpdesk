c2a3efa7da813cff9d5f014c3d0ca289e3b26738
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-05-23 17:10:09 -0300

    Criando um validador único

--- validador_acesso.php
+++ validador_acesso.php
@@ -1,6 +1,9 @@
 <?php
 
-session_start();
-  if (!isset($_SESSION['autenticado']) || $_SESSION['autenticado']) {
+  session_start();
+
+  if (!isset($_SESSION['autenticado']) || $_SESSION['autenticado'] != 'SIM') {
     header("Location: index.php?login=erroDeSecao");
   }
+
+?>
