03291d4d5c4284507ecd962b9f91ed97ed95b97f
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-05-23 06:00:47 -0300

    Definindo as páginas da seção

--- abrir_chamado.php
+++ abrir_chamado.php
@@ -1,3 +1,9 @@
+<?php
+  session_start();
+  if (!isset($_SESSION['autenticado']) || $_SESSION['autenticado']) {
+    header("Location: index.php?login=erroDeSecao");
+  }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
