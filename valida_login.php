99648ad78edc08a66b6157f40a8ef76442fe5a93
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-05-23 03:09:40 -0300

    Criando a validação para a autenticação do usuário

--- valida_login.php
+++ valida_login.php
@@ -1,11 +1,18 @@
 <?php
 
-echo '<pre>';
-print_r($_POST);
-echo '</pre>';
+$usuarios_app = [
+    ['email' => 'contato@marcosaleixo.com.br', 'senha' => '1234'],
+    ['email' => 'user@teste.com.br', 'senha' => 'abcd']
+];
 
-echo '<pre>';
-echo $_POST['email'];
-echo '<br>';
-echo $_POST['senha'];
-echo '</pre>';
+foreach ($usuarios_app as $user) {
+    if ($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
+        $usuario_autenticado = true;
+    }    
+}
+
+if ($usuario_autenticado) {
+    echo 'Usuário autenticado.';
+} else {
+    header("Location: index.php?login=erro");
+}
